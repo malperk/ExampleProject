4bb37ad4f7477fd2817034477cfe9fe6fc1ed748
Alper Karatas <alper_karatas@discovery.com>
2020-03-04 20:13:20 +0000

    Basic Routing

--- ExampleApp/ExampleApp/Connection/ConnectionInteractor.swift
+++ ExampleApp/ExampleApp/Connection/ConnectionInteractor.swift
@@ -12,6 +12,7 @@ import Foundation
 final class ConnectionInteractor: AnonymousServerConnecting & ViewStateProviding {
     
     let networking: NetworkingProtocol
+    let router: MainRouter
     
     // State Management
     var viewState: ViewState = .empty {
@@ -21,13 +22,21 @@ final class ConnectionInteractor: AnonymousServerConnecting & ViewStateProviding
     var onViewStateChange: ((ViewState) -> ())?
     
     
-    init(networking: NetworkingProtocol) {
+    init(networking: NetworkingProtocol, router: MainRouter) {
         self.networking = networking
+        self.router = router
     }
     
     func connect(ipAddress: String) {
-        networking.connectToServer(ipAddress: ipAddress, credentials: nil) { response in
-            
+        networking.connectToServer(ipAddress: ipAddress, credentials: nil) { [weak self] response in
+            if response.code == 200 {
+                self?.router.route(to: .confirmation)
+            } else if response.code == 401 {
+                self?.router.route(to: .login(ipAddress))
+            } else {
+                let error = AppError(innerError: nil, message: response.message)
+                self?.viewState = .error(error)
+            }
         }
         
     }
---
+++ ExampleApp/ExampleApp/Routing/Routing.swift
@@ -0,0 +1,40 @@
+//
+//  Routing.swift
+//  ExampleApp
+//
+//  Created by Alper Karatas on 04/03/2020.
+//  Copyright © 2020 Alper Karatas. All rights reserved.
+//
+
+import UIKit
+
+protocol Routing {
+
+    associatedtype Route
+
+    func route(to route: Route)
+
+}
+
+
+final class MainRouter: Routing {
+
+    private var navigationController: UINavigationController?
+
+    enum Route {
+        case login(String)
+        case confirmation
+    }
+
+    init(window: UIWindow?) {
+
+    }
+
+    func route(to route: Route) {
+
+    }
+
+    private func push(viewController: UIViewController) {
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+}
