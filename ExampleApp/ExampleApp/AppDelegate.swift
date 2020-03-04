a35f2105b023469f6f2fe2de5efbdef52aa39e34
Alper Karatas <alper_karatas@discovery.com>
2020-03-04 18:49:57 +0000

    Empty XCode project

---
+++ ExampleApp/ExampleApp/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  ExampleApp
+//
+//  Created by Alper Karatas on 04/03/2020.
+//  Copyright © 2020 Alper Karatas. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
