ede24e05644f02a2df37258df7f3d763c090a20b
Alper Karatas <alper_karatas@discovery.com>
2020-03-04 20:59:11 +0000

    ConfirmationViewController

---
+++ ExampleApp/ExampleApp/Screens/Confirmation/ConfirmationViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ConfirmationViewController.swift
+//  ExampleApp
+//
+//  Created by Alper Karatas on 04/03/2020.
+//  Copyright © 2020 Alper Karatas. All rights reserved.
+//
+
+import UIKit
+
+class ConfirmationViewController: UIViewController {
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
