db37ade1267581b0ed062037a1e1dac26edf7446
Alper Karatas <alper_karatas@discovery.com>
2020-03-04 19:01:45 +0000

    rename from ipsave to connect

---
+++ ExampleApp/ExampleApp/Connection/ConnectionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  IPSaveViewController.swift
+//  ExampleApp
+//
+//  Created by Alper Karatas on 04/03/2020.
+//  Copyright © 2020 Alper Karatas. All rights reserved.
+//
+
+import UIKit
+
+class ConnectionViewController: UIViewController {
+   
+    @IBOutlet private var ipTextField: UITextField!
+    
+    
+    @IBAction private func connectClick(_ sender: Any) {
+    }
+    
+}
